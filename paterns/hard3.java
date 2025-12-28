0844544d5f1bc442ae2aeaccb4b23f8300de711b
Developer RS | Rishabh Sharma <164728448+developer-rs5@users.noreply.github.com>
2025-12-28 16:43:37 +0530

    .

---
+++ paterns/hard3.java
@@ -0,0 +1,18 @@
+package paterns;
+
+public class hard3 {
+    public static void main(String[] args) {
+        int n = 5;
+
+        for(int i=1;i<=n;i++){
+            int x =n-i;
+            for(int j=x;j>=1;j--){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print(i+" ");
+            }
+            System.out.println("");
+        }
+    }   
+}
