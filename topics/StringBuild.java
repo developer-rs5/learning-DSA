1c6b6157a4547e14313b024e8a2f82d66e804c9c
Developer RS | Rishabh Sharma <164728448+developer-rs5@users.noreply.github.com>
2026-01-03 20:41:53 +0530

    Update StringBuild.java

--- topics/StringBuild.java
+++ topics/StringBuild.java
@@ -4,18 +4,30 @@ public class StringBuild {
     public static void main(String[] args) {
         StringBuilder sc = new StringBuilder("HelloWorld");
 
-        int left = 0;
-        int right = sc.length() - 1;
+        // int left = 0;
+        // int right = sc.length() - 1;
 
-        while (left < right) {
-            char temp = sc.charAt(left);
-            sc.setCharAt(left, sc.charAt(right));
-            sc.setCharAt(right, temp);
+        // while (left < right) {
+        //     char temp = sc.charAt(left);
+        //     sc.setCharAt(left, sc.charAt(right));
+        //     sc.setCharAt(right, temp);
 
-            left++;
-            right--;
+        //     left++;
+        //     right--;
+        // }
+
+        // System.out.println(sc);
+
+        for(int i = 0 ;i<sc.length()/2;i++){
+            int front = i;
+            int back = sc.length() - 1 - i;
+
+            char frontChar = sc.charAt(front);
+            char backChar = sc.charAt(back);
+
+            sc.setCharAt(front, backChar);
+            sc.setCharAt(back, frontChar);
         }
-
         System.out.println(sc);
     }
 }
