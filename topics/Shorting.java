8f475593495ebc3598a2a4afc575703c9fd210d2
Developer RS | Rishabh Sharma <164728448+developer-rs5@users.noreply.github.com>
2026-01-09 12:57:19 +0530

    .

--- topics/Shorting.java
+++ topics/Shorting.java
@@ -2,21 +2,45 @@ public class Shorting {
     public static void main(String[] args) {
         int arr[] = { 9, 8, 7, 6, 5, 4, 3, 2, 1 };
 
-        for (int i = 0; i < arr.length - 1; i++) {
-            for (int j = 0; j < arr.length - i - 1; j++) {
-                if (arr[j] > arr[j + 1]) {
-                    int temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
+        // for (int i = 0; i < arr.length - 1; i++) {
+        // for (int j = 0; j < arr.length - i - 1; j++) {
+        // if (arr[j] > arr[j + 1]) {
+        // int temp = arr[j];
+        // arr[j] = arr[j + 1];
+        // arr[j + 1] = temp;
 
-                }
+        // }
+        // }
+        // }
+        // printArr(arr);
+
+        // for (int i = 0; i < arr.length - 1; i++) {
+        //     int smallest = i;
+        //     for (int j = i + 1; j < arr.length; j++) {
+        //         if (arr[smallest] > arr[j]) {
+        //             smallest = j;
+        //         }
+        //     }
+        //     int temp = arr[smallest];
+        //     arr[smallest] = arr[i];
+        //     arr[i] = temp;
+        // }
+        // printArr(arr);
+
+        for(int i=1;i<arr.length;i++){
+            int current = arr[i];
+            int j = i-1;
+            while (j>=0 && current<arr[j]) {
+                arr[j+1]=arr[j];
+                j--;                
             }
+            arr[j+1]=current;
         }
         printArr(arr);
     }
 
     public static void printArr(int arr[]) {
-        for (int i = 0; i < arr.length - 1; i++) {
+        for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
         }
     }
